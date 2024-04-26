8a7e1187478824c0d900ca61781aa6aeb5e544dc
aleksbrain91 <oleksandr25popov@gmail.com>
2024-04-26 15:03:46 -0500

    classwork

--- test/test_case_10_Reports_and_Analytics.py
+++ test/test_case_10_Reports_and_Analytics.py
@@ -7,13 +7,17 @@
 # TODO: Click Save
 # TODO: Verify that New Folder appeared in the list of folders
 
+from helpers.utils import Utils
+
+random_name = Utils.generate_random_string()
+
 def test_case_10_verify_folders_creation_functionality(app):
     app.orangeHrm.open_application_and_login()
     app.orangeHrm.sideMenu.click_on_side_menu_button("Reports and Analytics")
     app.orangeHrm.reportsAnalytics.wait_for_page_loading()
     app.orangeHrm.reportsAnalytics.click_on_new_folder_button()
-    app.orangeHrm.popUpreportsAnalytics.input_new_folder_name("NewFolder")
+    app.orangeHrm.popUpreportsAnalytics.input_new_folder_name(random_name)
     app.orangeHrm.popUpreportsAnalytics.click_on_save_button()
     app.assert_that(app.orangeHrm.reportsAnalytics.get_succcess_meassage_text()).contains("Successfully Saved")
     app.orangeHrm.reportsAnalytics.wait_for_page_loading()
-    app.assert_that(app.orangeHrm.reportsAnalytics.get_folder_names_list()).contains("NewFolder")
+    app.assert_that(app.orangeHrm.reportsAnalytics.get_folder_names_list()).contains(random_name)
